09e5260d7909a243a069f0a41d3a415f541dc1ca
NigelTanYong <nigel.ty28@gmail.com>
2024-01-24 09:25:46 +0800

    initialised

---
+++ learningcoreml/learningcoreml/learningcoremlApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningcoremlApp.swift
+//  learningcoreml
+//
+//  Created by Nigel Tan Yong on 24/1/24.
+//
+
+import SwiftUI
+
+@main
+struct learningcoremlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
