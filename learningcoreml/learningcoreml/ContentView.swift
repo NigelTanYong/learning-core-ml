f7356bd37903c128d46f14fdd78bc80f9e2333dc
NigelTanYong <nigel.ty28@gmail.com>
2024-01-24 11:57:21 +0800

    imagepicker version

--- learningcoreml/learningcoreml/ContentView.swift
+++ learningcoreml/learningcoreml/ContentView.swift
@@ -6,19 +6,87 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
+    @State private var predictionText: String = "Predicted digit: ?"
+    @State private var isImagePickerPresented: Bool = false
+    @State private var selectedImage: UIImage?
+
+    private let modelManager = MyModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Select Image") {
+                isImagePickerPresented.toggle()
+            }
+            .sheet(isPresented: $isImagePickerPresented) {
+                ImagePicker(image: $selectedImage)
+            }
+            .padding()
+
+            if let selectedImage = selectedImage {
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+                    .padding()
+
+                Text(predictionText)
+                    .padding()
+                    .foregroundColor(.black)
+            }
         }
-        .padding()
+        .onChange(of: selectedImage) { newImage in
+                    // Call the predict method with the selected image
+            if let prediction = modelManager.predict(image: (newImage?.cgImage)!) {
+                        // Handle the prediction (e.g., update UI)
+                        print("Predicted digit: \(prediction)")
+                        predictionText = "Predicted digit: \(prediction)"
+                    }
+                }
     }
 }
 
-#Preview {
-    ContentView()
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var image: UIImage?
+
+        init(image: Binding<UIImage?>) {
+            _image = image
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                image = uiImage
+            }
+
+            picker.dismiss(animated: true, completion: nil)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(image: $image)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ learningcoreml/learningcoreml/MyModel.swift
@@ -0,0 +1,96 @@
+//
+//  MyModel.swift
+//  learningcoreml
+//
+//  Created by Nigel Tan Yong on 24/1/24.
+//
+
+import CoreML
+
+class MyModel {
+    // The Core ML model instance
+    var model: MNISTClassifier
+    
+    // Initializer
+    init() {
+        // Load the Core ML model
+        guard let loadedModel = try? MNISTClassifier(configuration: MLModelConfiguration()) else {
+            fatalError("Failed to load Core ML model")
+        }
+        self.model = loadedModel
+    }
+    // Make predictions with the model
+    // Make predictions with the model
+        func predict(image: CGImage) -> Int? {
+            do {
+                let pixelBuffer = try pixelBuffer(from: image)
+                let input = try MNISTClassifierInput(image: pixelBuffer)
+                let output = try model.prediction(input: input)
+                return Int(output.classLabel)
+            } catch {
+                print("Error making prediction: \(error)")
+                return nil
+            }
+        }
+
+        // Helper function to convert CGImage to CVPixelBuffer
+    private func pixelBuffer(from image: CGImage) throws -> CVPixelBuffer {
+        let targetSize = CGSize(width: 28, height: 28)  // Set the target size
+
+        guard let resizedImage = image.resize(to: targetSize) else {
+            throw NSError(domain: "com.example.MyModel", code: 1, userInfo: nil)
+        }
+
+        let pixelFormat = kCVPixelFormatType_OneComponent8
+
+        let options: [String: Any] = [
+            kCVPixelBufferCGImageCompatibilityKey as String: kCFBooleanTrue,
+            kCVPixelBufferCGBitmapContextCompatibilityKey as String: kCFBooleanTrue,
+            kCVPixelBufferPixelFormatTypeKey as String: pixelFormat
+        ]
+
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         Int(targetSize.width),
+                                         Int(targetSize.height),
+                                         pixelFormat,
+                                         options as CFDictionary,
+                                         &pixelBuffer)
+
+        guard status == kCVReturnSuccess, let buffer = pixelBuffer else {
+            throw NSError(domain: "com.example.MyModel", code: 1, userInfo: nil)
+        }
+
+        CVPixelBufferLockBaseAddress(buffer, [])
+        let context = CGContext(data: CVPixelBufferGetBaseAddress(buffer),
+                                width: CVPixelBufferGetWidth(buffer),
+                                height: CVPixelBufferGetHeight(buffer),
+                                bitsPerComponent: 8,
+                                bytesPerRow: CVPixelBufferGetBytesPerRow(buffer),
+                                space: CGColorSpaceCreateDeviceGray(),
+                                bitmapInfo: CGImageAlphaInfo.none.rawValue)
+
+        context?.draw(resizedImage, in: CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height))
+        CVPixelBufferUnlockBaseAddress(buffer, [])
+
+        return buffer
+    }
+
+    
+}
+extension CGImage {
+    func resize(to size: CGSize) -> CGImage? {
+        let context = CGContext(data: nil,
+                                width: Int(size.width),
+                                height: Int(size.height),
+                                bitsPerComponent: bitsPerComponent,
+                                bytesPerRow: 0,
+                                space: colorSpace ?? CGColorSpace(name: CGColorSpace.sRGB)!,
+                                bitmapInfo: bitmapInfo.rawValue)
+
+        context?.interpolationQuality = .high
+        context?.draw(self, in: CGRect(origin: .zero, size: size))
+
+        return context?.makeImage()
+    }
+}
